aa48648021f850ada9f77e5bede126f58dd03a98
Elias <pinheirinho2004@gmail.com>
2022-12-23 11:49:21 -0300

    improvement: apertar nas perguntas frequentes agora fecha a aberta anteriormente

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,10 @@ function showAnswer(element) {
     let li = element.parentElement
     let answer = li.querySelector(`[data-answer-id='${id}']`)
 
+    let ul = li.parentElement
+    let allAnswers = ul.querySelectorAll(`.faq-answer`)
+
+
     if (closed == "true") {
         answer.style.display = "block"
         closed = "false"
@@ -12,6 +16,15 @@ function showAnswer(element) {
         answer.style.display = "none"
         closed = "true"
     }
+
+    allAnswers.forEach(alpha => {
+        if (alpha.dataset.answerId != id) {
+            alpha.style.display = "none"
+            ul.querySelector(`[data-question-id='${alpha.dataset.answerId}']`).dataset.answerClosed = "true"
+        }
+    })
+
+
 
     element.dataset.answerClosed = closed
 }
